98ec5c7fa5ae3d727129fec34e8eef865947ae75
linnielin <madisonlin5370@gmail.com>
2021-02-20 16:24:49 -0500

    person class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,12 @@
+import java.util.HashMap;
+
 public class Person {
+  private int id;
+  private String firstName;
+  private String lastName;
+  private boolean isShopping;
+  private HashMap moneyOwed;
+  private HashMap moneyCollect;
+
+
 }
